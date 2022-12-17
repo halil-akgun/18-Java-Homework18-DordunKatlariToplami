eb98f277a8cb91e8ff6192c501736f5773c5f1cf
halil <halilakgun0@gmail.com>
2022-12-17 18:45:35 +0300

    açıklama ekledim.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        Ödev
+        Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve
+        4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+         */
+
         Scanner input = new Scanner(System.in);
         int total = 0;
         int a = 0;
